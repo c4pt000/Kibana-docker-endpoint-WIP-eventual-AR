01bc9e54a984111c9561d788f6d39f64ad77b979
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2021-07-23 10:34:37 -0400

    Update create-kibana.sh

--- create-kibana.sh
+++ create-kibana.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
-echo "cd to a directory with VM images like KVM_IMAGES or a VM directory on your host"
 yum install docker-compose -y
+ 
+
+
 cd 
 mkdir elasticsearch-kibana-setup 
 cd elasticsearch-kibana-setup 
@@ -10,7 +11,7 @@ echo '
 version: '3'
 services:
   elasticsearch:
-    image: docker.elastic.co/elasticsearch/elasticsearch:7.13.4
+    image: docker.elastic.co/elasticsearch/elasticsearch:6.8.0
     ports:
       - 9200:9200
     volumes:
@@ -19,7 +20,7 @@ services:
   kibana:
     depends_on:
       - elasticsearch  
-    image: docker.elastic.co/kibana/kibana:7.13.4
+    image: docker.elastic.co/kibana/kibana:6.8.0
     ports:
       - 5601:5601
     volumes:
@@ -37,5 +38,44 @@ echo '
 server.name: kibana
 server.host: "0"
 elasticsearch.hosts: [ "http://elasticsearch:9200" ] ' > kibana.yml
+
+docker-compose up
+
+docker-compose exec elasticsearch bash
+bin/elasticsearch-certutil ca
+echo "enter, enter, enter here"
+bin/elasticsearch-certutil cert --ca elastic-stack-ca.p12
+
+echo '
+version: '3'
+services:
+  elasticsearch:
+    image: docker.elastic.co/elasticsearch/elasticsearch:6.8.0
+    ports:
+      - 9200:9200
+    volumes:
+      - ./elasticsearch.yml:/usr/share/elasticsearch/config/elasticsearch.yml
+      - ./elastic-certificates.p12:/usr/share/elasticsearch/config/elastic-certificates.p12
+
+  kibana:
+    depends_on:
+      - elasticsearch
+    image: docker.elastic.co/kibana/kibana:6.8.0
+    ports:
+      - 5601:5601
+    volumes:
+      - ./kibana.yml:/usr/share/kibana/config/kibana.yml
+' > docker-compose.yml
+
+echo '
+cluster.name: my-elasticsearch-cluster
+network.host: 0.0.0.0
+xpack.security.enabled: true
+xpack.security.transport.ssl.enabled: true
+xpack.security.transport.ssl.keystore.type: PKCS12
+xpack.security.transport.ssl.verification_mode: certificate
+xpack.security.transport.ssl.keystore.path: elastic-certificates.p12
+xpack.security.transport.ssl.truststore.path: elastic-certificates.p12
+xpack.security.transport.ssl.truststore.type: PKCS12 ' > elasticsearch.yml 
 
 docker-compose up
